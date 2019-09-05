66a38410f790618f37a6d82194324ff3849dfb4d
Gatlin Carrier <gatlin.carrier@gmail.com>
2019-09-05 17:31:17 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-let cards = ["queen", "queen", "king", "kings"];
+let cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -11,9 +33,11 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId.rank]);
 
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suit)
+  console.log(cards[cardId].cardImage)
 
   checkForMatch();
 }
