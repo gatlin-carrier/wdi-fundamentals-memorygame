ebe0e48d615b99162b2b06f3ec7795ec80ac91f6
Gatlin Carrier <gatlin.carrier@gmail.com>
2019-09-18 14:29:02 -0400

    game logic added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-let cards = [
+var cards = [
   {
     rank: "queen",
     suit: "hearts",
@@ -21,29 +21,41 @@ let cards = [
   }
 ];
 
-let cardsInPlay = [];
+var cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 }
 
-function flipCard(cardId) {
+function flipCard() {
 
-  cardsInPlay.push(cards[cardId.rank]);
-
-  console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].suit)
-  console.log(cards[cardId].cardImage)
-
-  checkForMatch();
+  var cardId = this.getAttribute('data-id');
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cardsInPlay);
+  this.setAttribute('src', cards[cardId].cardImage);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
 }
 
-flipCard(0);
-flipCard(2);
+
+
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src", "./images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+
+  }
+}
+
+createBoard();
 
 
 
